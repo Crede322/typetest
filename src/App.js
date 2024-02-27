38af0921c15d58186e087112ec481826e72fda40
Riiru <burhanov774@gmail.com>
2024-02-27 07:33:27 +0600

    добавил ещё кода к адаптации, написал пару строчек комментариев

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
-import "./reset.css"
-import "./common.css"
-import classes from "./components/Mainstyles.module.css"
-import Menu from './components/Menu';
-import { TabContextProvider } from './context/TabContext.tsx';
+import logo from "./logo.svg";
+import "./reset.css";
+import "./common.css";
+import classes from "./components/Mainstyles.module.css";
+import Menu from "./components/Menu";
+import { TabContextProvider } from "./context/TabContext.tsx";
 
 function App() {
   return (
     <div className="App">
+      {/* элемент если экран меньше чем 1366px */}
+      <div className={classes.empty_block} />
       <TabContextProvider>
         <div className={classes.main}>
           <Menu />
